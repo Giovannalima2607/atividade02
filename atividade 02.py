669b8022e3b778675796dbb8027a9434688aa5ae
Giovannalima2607 <giovannasilva123@gmail.com>
2024-09-09 09:48:23 -0300

    digite a temperatura em celsius

---
+++ atividade 02.py
@@ -0,0 +1,6 @@
+c = float(input("digite a temperatura em celsius:"))
+f = 32 + (9/5)*c
+k = c + 273
+print("A temperatura em celsius é:",c,"°c")
+print("A temperatura em fahrenheit é:",f,"ºf")
+print("A temperatura em kelvin é:",k,"k")
